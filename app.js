f58eec45f228074faaea2380b8045e71602cf362
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-05-24 23:03:15 +0530

    Done

---
+++ app.js
@@ -0,0 +1,29 @@
+var clickButton = document.querySelector("#btn-default");
+var inputText = document.querySelector("#input");
+var outputDiv = document.querySelector("#output");
+
+var url ="https://api.funtranslations.com/translate/morse.json";
+
+function urlTranslation(input){  
+
+return url + "?" + "text=" + input;
+}
+
+
+
+function clickHandler() {
+   
+    var textInput = inputText.value;
+
+fetch(urlTranslation(textInput))
+   .then(res => res.json())
+   .then(json => {
+       var traslatedText = json.contents.translated;
+       outputDiv.innerText = traslatedText;
+   })
+   .catch(error => alert("something wrong ladies and gentleman please try again later")
+   )
+
+}
+
+clickButton.addEventListener("click", clickHandler);
